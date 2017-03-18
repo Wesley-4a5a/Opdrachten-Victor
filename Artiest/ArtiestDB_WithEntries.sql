59cbaad1aa2f7480a160c1ededc4e6d52a2f0ad1
Wesley-4a5a <374053@edu.rocmn.nl>
2017-03-18 11:47:06 +0100

    Artiest with TestData

---
+++ Artiest/ArtiestDB_WithEntries.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 18, 2017 at 11:44 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ArtiestDB`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+-- in use(#1932 - Table 'artiestdb.album' doesn't exist in engine)
+-- Error reading data: (#1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ArtiestDB`.`album`' at line 1)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artiest`
+--
+-- in use(#1932 - Table 'artiestdb.artiest' doesn't exist in engine)
+-- Error reading data: (#1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ArtiestDB`.`artiest`' at line 1)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genre`
+--
+-- in use(#1932 - Table 'artiestdb.genre' doesn't exist in engine)
+-- Error reading data: (#1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ArtiestDB`.`genre`' at line 1)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genre_has_album`
+--
+-- in use(#1932 - Table 'artiestdb.genre_has_album' doesn't exist in engine)
+-- Error reading data: (#1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ArtiestDB`.`genre_has_album`' at line 1)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `platenlabel`
+--
+-- in use(#1932 - Table 'artiestdb.platenlabel' doesn't exist in engine)
+-- Error reading data: (#1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ArtiestDB`.`platenlabel`' at line 1)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studio`
+--
+-- in use(#1932 - Table 'artiestdb.studio' doesn't exist in engine)
+-- Error reading data: (#1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ArtiestDB`.`studio`' at line 1)
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
