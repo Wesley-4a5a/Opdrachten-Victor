880fb32fdc0174767328baa9c99df4fcc195a0a3
Koning, W. (Wesley) <374053@edu.rocmn.nl>
2017-05-10 08:32:11 +0200

    teller

---
+++ MATWENT/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Document</title>
+  <style>
+    body{
+      background-color: RGBA(036,056,196,0.8);
+    }
+
+    #logoke{
+      width: 10vw;
+      height: 15vh;
+    }
+  </style>
+</head>
+<body>
+
+    <div>
+      MA Twente Registratie Systeem
+    </div>
+
+<img id='logoke' src='logo.png'>
+<hr />
+Incident Melden - Incident Inzien - Incident Afhandeling - Gebruikersbeheer - Configuratiebeheer
+<hr />
+</body>
+</html>
