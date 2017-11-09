e918116ea529407c7a89dd0081d481ff8106715b
Koning, W. (Wesley) <374053@edu.rocmn.nl>
2017-11-09 12:40:01 +0100

    SeperatedJS

--- Jaar3/git-wizard/git-wizard.js
+++ Jaar3/git-wizard/git-wizard.js
@@ -13,37 +13,31 @@
 
     var currentpage = 0;
 
-    function PageUp()
-    {
+    function PageUp() {
         currentpage++;
         buttonCheck();
     }
 
-    function PageDown()
-    {
+    function PageDown() {
         currentpage--;
         buttonCheck();
     }
 
-    function buttonCheck(){
-      var prev = document.querySelector('#previousStep');
-      var next = document.querySelector('#nextStep');
-      if(currentpage == 0){
-        prev.disabled = true;
-      }
-      else if(currentpage == 4){
-        next.disabled = true;
-      }
-      else{
-        prev.disabled = false;
-        next.disabled = false;
-      }
-      pageInput();
+    function buttonCheck() {
+        var prev = document.querySelector('#previousStep');
+        var next = document.querySelector('#nextStep');
+        if (currentpage == 0) {
+            prev.disabled = true;
+        } else if (currentpage == 4) {
+            next.disabled = true;
+        } else {
+            prev.disabled = false;
+            next.disabled = false;
+        }
     }
 
-    function getContent()
-    {
-        xhr.open("GET", 'views/Stap' + currentpage + '.html' );
+    function getContent() {
+        xhr.open("GET", 'views/Stap' + currentpage + '.html');
         xhr.send();
     }
 
@@ -51,6 +45,7 @@
     function ajaxLoad(event) {
         if (this.status === 200) {
             document.querySelector('.steps').innerHTML = this.responseText;
+            pageInput();
         } else {
             console.log("unsuccessful request");
         }
@@ -60,31 +55,72 @@
         console.log('error');
     }
 
-function pageInput(){
-  switch (currentpage) {
-    case 0:
-      console.log('0');
-      break;
-    case 1:
-      document.querySelector('#folder').addEventListener("click", folderSearch);
-      break;
-    case 2:
-      console.log('2');
-      break;
-    case 3:
-      console.log('3');
-      break;
-    case 4:
-      console.log('4');
-      break;
-    default:
+    function pageInput() {
+        switch (currentpage) {
+            case 0:
+                console.log('0');
+                break;
+            case 1:
+                document.querySelector('#folder').addEventListener("keyup", folderSearch);
 
-  }
-}
+                function folderSearch() {
+                    var search = document.getElementById('folder').value;
+                    if (search.indexOf(' ') > 0) {
+                        search = '"' + search + '"';
+                    }
+                    document.querySelector('#zoekFolderCommando').innerHTML = 'cd ' + search;
+                }
+                document.querySelector('#cloneUrl').addEventListener("keyup", gitSearch);
 
-// var search = document.querySelector('#folder').value;
-function folderSearch(){
-  console.log('input searh werkt');
-  // document.querySelector('#zoekFolderCommando').innerHTML = search;
-}
-// console.log(search)
+
+                function gitSearch() {
+                    var search = document.getElementById('cloneUrl').value;
+                    if (search.indexOf(' ') > 0) {
+                        search = '"' + search + '"';
+                    }
+                    document.querySelector('#cloneCommand').innerHTML = 'git clone ' + search;
+                }
+
+                document.querySelector('#zoekFolderCommando').addEventListener("click", copyStringToClipboard);
+                document.querySelector('#cloneCommand').addEventListener("click", copyStringToClipboard);
+
+                break;
+            case 2:
+                document.querySelector('#commitComment').addEventListener("keyup", addComment);
+
+                function addComment() {
+                    var search = document.getElementById('commitComment').value;
+                    document.querySelector('#commitCommand').innerHTML = 'git commit -a -m "' + search + '"';
+                }
+
+                document.querySelector('#getStatus').addEventListener("click", copyStringToClipboard);
+                document.querySelector('#getAdd').addEventListener("click", copyStringToClipboard);
+                document.querySelector('#commitCommand').addEventListener("click", copyStringToClipboard);
+
+                break;
+            case 3:
+                document.querySelector('#getPull').addEventListener("click", copyStringToClipboard);
+
+                break;
+            case 4:
+                document.querySelector('#getPush').addEventListener("click", copyStringToClipboard);
+
+                break;
+            default:
+
+        }
+    }
+
+    function copyStringToClipboard() {
+        var copytje = this.innerHTML;
+
+        function handler(event) {
+            event.clipboardData.setData('text/plain', copytje);
+            event.preventDefault();
+            document.removeEventListener('copy', handler, true);
+        }
+
+        document.addEventListener('copy', handler, true);
+        document.execCommand('copy');
+    }
+    // console.log(search)
