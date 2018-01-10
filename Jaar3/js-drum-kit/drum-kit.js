63662582f2988de5822f2c8d8aea179018067fc9
Wesley-4a5a <374053@edu.rocmn.nl>
2018-01-10 08:29:24 +0100

    drum

--- Jaar3/js-drum-kit/drum-kit.js
+++ Jaar3/js-drum-kit/drum-kit.js
@@ -3,9 +3,14 @@
 
 
 
-document.body.addEventListener('keydown', function(e){aap(e)});
+document.body.addEventListener('keydown', function(e){down(e);});
+document.body.addEventListener('keyup', function(e){up(e);});
 
-
-function aap(e){
+function down(e){
+    document.querySelector('div[data-key="'+e.keyCode+'"]').classList.add('playing','sound');
     document.querySelector('audio[data-key="'+e.keyCode+'"]').play();
+}
+
+function up(e){
+    document.querySelector('div[data-key="'+e.keyCode+'"]').classList.remove('playing','sound');
 }
