979f8c6f02daf832aadbfeba99a2b8d3903839bb
Wesley-4a5a <374053@edu.rocmn.nl>
2018-01-10 08:45:50 +0100

    wakkamoooo

--- Jaar3/whack-a-mole/script.js
+++ Jaar3/whack-a-mole/script.js
@@ -10,14 +10,23 @@ startButton.addEventListener('click', function () {
 });
 
 
+
 function startGame() {
     console.log('test');
 
     setInterval(function(){
       var hond = Math.floor((Math.random() * 6) + 1);
        holes[hond - 1].className = "hole up hole" + hond;
+       holes[hond - 1].addEventListener('click', function(){score();});
       setTimeout(function(){
        holes[hond - 1].className = "hole hole" + hond;
-     }, 250);
-   }, 250);
+     }, 1000);
+   }, 1000);
+}
+
+var scoreCounter = 0;
+function score(){
+  scoreCounter++;
+  console.log(scoreCounter);
+  document.querySelector('.score').innerHTML = scoreCounter;
 }
