b8a5bd472e87df8c48c510fc6b4c132a373e4284
Koning, W. (Wesley) <374053@edu.rocmn.nl>
2018-01-11 11:19:03 +0100

    factory

---
+++ Jaar3/ShapeFactory/shapeFactoryClass.php
@@ -0,0 +1,12 @@
+<?php
+
+VehicleFactory Class -> create function if statement 'car' of 'bicycle'
+
+Vehicle Class -> move function
+
+Car extends Class -> move function
+Bicycle extends Class -> move function
+
+
+
+ ?>
